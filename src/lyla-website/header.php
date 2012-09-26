21c84949cd7eaf1f906e78452f5c96c22ff3e577
PTKDev <ptkdev@gmail.com>
2012-09-26 18:15:12 +0200

    [add] index, css, js, plugin, mobile script, language script

---
+++ src/lyla-website/header.php
@@ -0,0 +1,74 @@
+<?php 
+/*
+ * ChiaraDeLiberato (Wordpress Theme)
+ *   Copyright (C) 2012  Chiara De Liberato
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * 
+ *	WebSite: http://www.chiaradeliberato.it/
+ * 	Source Code: http://source.chiaradeliberato.it/ 
+ * 
+*/
+?>
+<!DOCTYPE html> 
+<html dir="ltr" lang="<?php echo $lang0; ?>"> 
+<head> 
+	<meta charset="<?php echo $lang2; ?>" />
+	<title><?php echo $lang3; ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<meta name="generator" content="Geany IDE" />
+	
+	<meta name="author" content="Patryk Rzucidlo" />
+	<meta name="author-mail" content="ptkdev[at]gmail[dot]com" />
+	<meta name="author-msn" content="ptkdev[at]gmail[dot]com" />
+	<meta name="author-skype" content="ptkdev" />
+	<meta name="author-twitter" content="@PTKDev" />
+	<meta name="author-irc" content="chat.freenode.net #ptkdev" />
+	<meta name="author-gadu_gadu" content="2825253" />
+	<meta name="author-phone_wind" content="+39 3281759093" />
+	
+	<meta name="copyright" content="GNU Affero General Public License" />
+	<meta name="source-code" content="https://source.chiaradeliberato.it/" />
+	
+	<meta name="distribution" content="global" />
+	<meta name="robots" content="index,follow" />
+	<meta name="revisit-after" content="3 days" />
+	<meta name="rating" content="general" />
+	<meta name="language" content="it" />
+	
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link rel="alternate" type="application/rss+xml" title="<?php echo $lang3; ?> - <?php echo $lang4; ?>" href="http://feeds.feedburner.com/chiaradeliberato" />
+	<link rel="shortcut icon" href="http://<?php echo $_SERVER['SERVER_NAME']; ?>/favicon.ico" />
+	<link rel="icon" href="http://<?php echo $_SERVER['SERVER_NAME']; ?>/favicon.png" type="image/png" />
+	<link rel="icon" href="http://<?php echo $_SERVER['SERVER_NAME']; ?>/favicon.ico" type="image/ico" />
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	
+	<link rel="stylesheet" type="text/css" href="./css/desktop/layout.css" />
+	<link rel="stylesheet" type="text/css" href="./css/desktop/header.css" />
+	<link rel="stylesheet" type="text/css" href="./css/desktop/body.css" />
+	<link rel="stylesheet" type="text/css" href="./css/desktop/footer.css" />
+	<link rel="stylesheet" type="text/css" href="./css/desktop/link.css" />
+	
+	<link href="./plugin/lightbox/lightbox.css" rel="stylesheet" type="text/css" media="screen" /> 
+	
+	<script src="./plugin/lightbox/js/prototype.js" type="text/javascript"></script> 
+	<script src="./plugin/lightbox/js/scriptaculous.js?load=effects,builder" type="text/javascript"></script> 
+	<script src="./plugin/lightbox/js/lightbox.js" type="text/javascript"></script>
+	<!--[if lt IE 7.]>
+	<script defer type="text/javascript" src="./js/pngfix.js"></script>
+	<![endif]-->
+</head>
+
+<body>
