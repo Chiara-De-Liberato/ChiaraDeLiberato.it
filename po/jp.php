442b276a9d49d81ae8b853a3352c83335f545fb3
PTKDev <ptkdev@gmail.com>
2012-09-26 15:20:53 +0200

    [new] po files (language)

---
+++ po/jp.php
@@ -0,0 +1,46 @@
+<?php 
+/*
+ * ChiaraDeLiberato (Wordpress Theme)
+ *   Copyright (C) 2012  Chiara De Liberato
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * 
+ *	WebSite: http://www.chiaradeliberato.it/
+ * 	Source Code: http://source.chiaradeliberato.it/ 
+ * 
+*/
+
+
+$lang1 = "";
+$lang2 = "";
+$lang3 = "";
+$lang4 = "";
+$lang5 = "";
+$lang6 = "";
+$lang7 = "";
+$lang8 = "";
+$lang9 = "";
+$lang10 = "";
+$lang11 = "";
+$lang12 = "";
+$lang13 = "";
+$lang14 = "";
+$lang15 = "";
+$lang16 = "";
+$lang17 = "";
+$lang18 = "";
+$lang19 = "";
+$lang20 = "";
+?>
